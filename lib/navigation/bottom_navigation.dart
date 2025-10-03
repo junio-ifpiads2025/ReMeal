6f028d39839551c83e36e9947b6761f0681a42ef
Junio-Alves <juniophb2004@gmail.com>
2025-10-02 21:02:23 -0300

    Criado bottomTabBar,Restaurant Page, Profile Page e model

---
+++ lib/navigation/bottom_navigation.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:remeal/pages/profile_page.dart';
+import 'package:remeal/pages/restaurants_page.dart';
+
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  int _selectedIndex = 0;
+
+  // Lista de páginas
+  final List<Widget> _pages = const [
+    RestaurantsPages(),
+    ProfilePage()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex], // muda a tela conforme item selecionado
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex, // item atual
+        onTap: _onItemTapped, // ação ao clicar
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Perfil",
+          ),
+        ],
+      ),
+    );
+  }
+}
