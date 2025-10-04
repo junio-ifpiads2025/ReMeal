af3c0ae23a665b1a733e2c889b1f2d6ebc01e789
mafe <mfernandarocosta@gmail.com>
2025-10-04 13:46:32 -0300

    Ajustes no model restaurante para incluir endereco

--- lib/models/restaurant_model.dart
+++ lib/models/restaurant_model.dart
@@ -1,6 +1,7 @@
 class RestaurantModel {
   final String id;
   final String name;
+  final String address;
   final String description;
   final String imageUrl;
   final double rating;
@@ -8,6 +9,7 @@ class RestaurantModel {
   RestaurantModel({
     required this.id,
     required this.name,
+    required this.address,
     required this.description,
     required this.imageUrl,
     this.rating = 4.0, // Default rating
@@ -17,6 +19,7 @@ class RestaurantModel {
     return RestaurantModel(
       id: json['id'].toString(),
       name: json['nome'] ?? '',
+      address: json['endereco'] ?? '',
       description: json['descricao'] ?? '',
       imageUrl: json['imagem'] ?? '',
       rating: (json['rating'] as num?)?.toDouble() ?? 4.0,
@@ -27,6 +30,7 @@ class RestaurantModel {
     return {
       'id': id,
       'nome': name,
+      'endereco': address,
       'descricao': description,
       'imagem': imageUrl,
       'rating': rating,
