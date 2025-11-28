aeb72c0dd805da026c7d499722e47a5f24fe8bef
Junio-Alves <juniophb2004@gmail.com>
2025-11-27 21:31:53 -0300

    comentário

--- lib/interface/i_auth_repository.dart
+++ lib/interface/i_auth_repository.dart
@@ -1,6 +1,7 @@
 import 'package:remeal/models/user.dart';
 
-
+// Estou utilizando esta interface para que o meu repository possa ser substituível por outro tipo de repository, 
+// como uma repository de banco de dados ou firebase.
 // Classe abstrata = Contrato
 abstract class IAuthRepository {
   Future<User?> getCurrentUser(); // Recuperar usuário ao abrir o app
